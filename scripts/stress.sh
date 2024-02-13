a11f22095d5f2f5ad8e1fb5abe226f31241c2ce8
jaemin <datactress@gmail.com>
2024-02-13 23:10:45 +0900

    fix: Modify stress test to send plane InvokeTransactions randomly

--- scripts/stress.sh
+++ scripts/stress.sh
@@ -6,7 +6,11 @@ COUNT=10000
 for i in $(seq 0 $COUNT)
 do
     NONCE=$(printf "0x%x" $i)
-    ENCRYPT_DATA=$(cat <<EOF
+    RANDOM_CHOICE=$((RANDOM % 2))
+
+    if [ $RANDOM_CHOICE -eq 0 ]; then
+        # Encrypt and Add Encrypted Invoke Transaction
+        ENCRYPT_DATA=$(cat <<EOF
 {
   "jsonrpc": "2.0",
   "method": "starknet_encryptInvokeTransaction",
@@ -32,15 +36,13 @@ do
 EOF
 )
 
-    RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST --data "$ENCRYPT_DATA" $URL)
-    echo "Encrypt response: $RESPONSE"
+        RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST --data "$ENCRYPT_DATA" $URL)
+        echo "Encrypt response: $RESPONSE"
 
-    # encrypted_invoke_transaction from RESPONSE
-    ENCRYPTED_TRANSACTION=$(echo $RESPONSE | jq -c '.result.encrypted_invoke_transaction')
-    echo "Encrypted transaction: $ENCRYPTED_TRANSACTION"
+        ENCRYPTED_TRANSACTION=$(echo $RESPONSE | jq -c '.result.encrypted_invoke_transaction')
+        echo "Encrypted transaction: $ENCRYPTED_TRANSACTION"
 
-    # AddEncryptedInvokeTransaction
-    ADD_ENCRYPTED_DATA=$(cat <<EOF
+        ADD_ENCRYPTED_DATA=$(cat <<EOF
 {
   "jsonrpc": "2.0",
   "method": "starknet_addEncryptedInvokeTransaction",
@@ -52,7 +54,39 @@ EOF
 EOF
 )
 
-    ADD_RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST --data "$ADD_ENCRYPTED_DATA" $URL)
-    echo "Add encrypted transaction response: $ADD_RESPONSE"
+        ADD_RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST --data "$ADD_ENCRYPTED_DATA" $URL)
+        echo "Add encrypted transaction response: $ADD_RESPONSE"
+    else
+        # Add Invoke Transaction Directly
+        INVOKE_DATA=$(cat <<EOF
+{
+    "jsonrpc": "2.0",
+    "method": "starknet_addInvokeTransaction",
+    "params": {
+        "invoke_transaction": {
+            "sender_address":"0x0000000000000000000000000000000000000000000000000000000000000001",
+            "calldata": [
+                "0x0000000000000000000000000000000000000000000000000000000000001111",
+                "0x36fa6de2810d05c3e1a0ebe23f60b9c2f4629bbead09e5a9704e1c5632630d5",
+                "0x0"
+            ],
+            "type": "INVOKE",
+            "max_fee": "0xbc614e",
+            "version":"0x1",
+            "signature":[
+                "0x0",
+                "0x0"
+            ],
+            "nonce":"$NONCE",
+            "offset_version": "0x0"
+        }
+    },
+    "id": 1
+}
+EOF
+)
 
+        INVOKE_RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST --data "$INVOKE_DATA" $URL)
+        echo "Invoke transaction response: $INVOKE_RESPONSE"
+    fi
 done
